29215b354d68eb583bcbd312ac459da8133b7a28
James Njenga <knjenga94@gmail.com>
2022-08-03 12:24:10 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b){
+ let sum = a+b;
+ return sum;
+}
+
+function subtract(a,b){
+    let result=a-b;
+    return result;
+
+}
+function multiply(a,b){
+    let product=a*b;
+    return product;
+
+}
+function divide(a,b){
+    let division=a/b;
+    return division;
+
+}
+
+function increment(n){
+    return n+1;
+}
+
+function decrement(n){
+    return n-1;
+}
+
+function makeInt(n){
+   let res=parseInt(n,10);
+   return res;
+}
+
+function preserveDecimal(n){
+   return parseFloat(n);
+}
